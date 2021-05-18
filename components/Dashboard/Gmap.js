d5da7b0839ff47791dd0c6f1f8ea8ed1fd6fa903
Shubham Raj <shubham14p3@gmail.com>
2021-05-18 20:36:49 +0530

    Minor sample of work

---
+++ components/Dashboard/Gmap.js
@@ -0,0 +1,9 @@
+const Gmap = () => {
+    return (
+        <div className="">
+            <h2 className="text-4xl">Google Map Integration Testing</h2>
+        </div>
+    )
+}
+
+export default Gmap
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import dashboard from './dashboard';
+import Dashboard from './dashboard';
+
+export default function Home() {
+  return (
+    <div>
+        <Dashboard/>    
+    </div>
+  )
+}
